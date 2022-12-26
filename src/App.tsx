7fad7e7ce39b58fa4793f68537f5c8779b3da9e4
Andrei Hutsau <mwplt@mail.ru>
2022-12-26 23:28:02 +0300

    some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Todo} from "./Todo";
+import {Led} from "./led";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let minCount = 0
+    let maxCount = 5
+    let [count, setCount] = useState<any>(minCount)
+
+    const addCount = () => {
+        if (count < maxCount) {
+            setCount(++count)
+            console.log(count)
+        } else {
+            console.log('stop')
+        }
+    }
+
+    return (
+        <div className={'main'}>
+            <div className="App">
+                <div className={'inputSet'}>
+                </div>
+                <div className={'borderMain'}>
+                    <div className={'controls'}>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div className="App">
+                <div className={'led'}>
+                    <Led count={count}/>
+                </div>
+
+                <div className={'borderMain'}>
+                    <div className={'controls'}>
+                        <Todo addCount={addCount}
+                              setCount={setCount}
+                              count={count}
+                              minCount={minCount}
+                              maxCount={maxCount}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    );
 }
 
 export default App;

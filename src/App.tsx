562e25dd2f70f3db4a3bf45bec5c0282afb0de10
Andrei Hutsau <mwplt@mail.ru>
2022-12-28 22:17:45 +0300

    made rules of settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import {Todo} from "./Todo";
 import {Led} from "./led";
@@ -6,37 +6,53 @@ import './App.css'
 import {ButtonSuper} from "./ButtonSuper";
 
 function App() {
-    let [minCount,setMinCount]=useState<number>(0)
-    let [maxCount,setMaxCount]=useState<number>(5)
+    // let minCount=0
+    // let maxCount=5
+    let [minCount, setMinCount] = useState<number>(0)
+    let [maxCount, setMaxCount] = useState<number>(5)
     let [count, setCount] = useState<any>(minCount)
-    let [makeSettings,setMakeSettings]=useState<boolean>(false)
-    let[error,setError]=useState('')
+    let [makeSettings, setMakeSettings] = useState<boolean>(false)
+    let [error, setError] = useState('')
 
     const addCount = () => {
         if (count < maxCount) {
             setCount(++count)
         }
     }
-    let changeMinCountHandler=(e:ChangeEvent<HTMLInputElement>)=>{
-        if (+e.currentTarget.value>=0 && minCount<maxCount){
-            setMinCount(+e.currentTarget.value)
-        }else{
+    let changeMinCount=(minNum:number)=>{
+        console.log(`inside ${minNum}`)
+        if (minNum>=0 && maxCount!==minNum) {
+            setMinCount(minNum)
+        } else {
             setCount('not correct value')
         }
     }
-    let changeMaxCountHandler=(e:ChangeEvent<HTMLInputElement>)=>{
-        if (minCount<maxCount){
-            setMaxCount(+e.currentTarget.value)
-        }else{
+    let changeMaxCount=(maxNum:number)=>{
+        console.log(`inside max ${maxNum}`)
+        if (minCount!==maxNum) {
+            setMaxCount(maxNum)
+        } else {
             setCount('not correct value')
         }
+    }
+    let changeMinCountHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCount("input value and press set")
+        let currentNumber = +e.currentTarget.value
+        changeMinCount(currentNumber)
+    }
 
+    console.log(`min: ${minCount}`)
+    console.log(`max: ${maxCount}`)
+
+    let changeMaxCountHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCount("input value and press set")
+        changeMaxCount(+e.currentTarget.value)
     }
-    let onClickSetHandler=()=>{
+    let onClickSetHandler = () => {
         setCount(minCount)
         setMakeSettings(false)
     }
-    let onFocusHandler=()=>{
+    let onFocusHandler = () => {
         setMakeSettings(true)
         setCount("input value and press set")
     }
@@ -45,13 +61,15 @@ function App() {
         <div className={'main'}>
             <div className="App">
                 <div className={'inputSet'}>
-                    <div >
+                    <div>
                         <span>max value</span>
-                        <input value={maxCount} type={'number'} onChange={changeMaxCountHandler} onFocus={onFocusHandler}/>
+                        <input value={maxCount} type={'number'} onChange={changeMaxCountHandler}
+                               onFocus={onFocusHandler}/>
                     </div>
-                    <div >
+                    <div>
                         <span>start value</span>
-                        <input value={minCount} type={'number'} onChange={changeMinCountHandler} onFocus={onFocusHandler} />
+                        <input value={minCount} type={'number'} onChange={changeMinCountHandler}
+                               onFocus={onFocusHandler}/>
                     </div>
 
                 </div>
@@ -68,7 +86,7 @@ function App() {
                     <Led count={count}/>
                 </div>
 
-                <div className={'borderMain'} >
+                <div className={'borderMain'}>
                     <div className={'controls'}>
                         <Todo addCount={addCount}
                               setCount={setCount}

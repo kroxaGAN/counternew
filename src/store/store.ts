319580a1f778fc8cff6a241ff80a577ad9f2c091
Andrei Hutsau <mwplt@mail.ru>
2023-01-31 22:21:19 +0300

    update redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
-import { combineReducers, createStore } from 'redux'
+// import { combineReducers, createStore } from 'redux'
 import {MinCountReducers} from "../reducers/minCountReducers";
+import {combineReducers, legacy_createStore} from "redux";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -9,7 +10,7 @@ const rootReducer = combineReducers({
     // todolists: todolistsReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer)
+export const store = legacy_createStore(rootReducer)
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
